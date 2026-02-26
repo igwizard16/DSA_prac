3f693cd25bf0e21d73590a55c30e2caf925867f7
KESHAV MUNDEL <keshavjeemains2024@gmail.com>
2026-02-26 21:16:14 +0530

    Time: 0 ms (100%), Space: 10.5 MB (70.17%) - LeetHub

--- 0046-permutations/0046-permutations.cpp
+++ 0046-permutations/0046-permutations.cpp
@@ -1,27 +1,21 @@
 class Solution {
 public:
 
-    void permut(vector<int>& nums, vector<int>& ds, vector<vector<int>>& ans, vector<int> freq){
-        if(ds.size() == nums.size()){
-            ans.push_back(ds);
+    void recurPermut(int idx, vector<int>& nums, vector<vector<int>>& ans){
+        if(idx == nums.size()){
+            ans.push_back(nums);
             return;
         }
-        for(int i = 0; i < nums.size(); i++){
-            if(!freq[i]){
-                ds.push_back(nums[i]);
-                freq[i] = 1;
-                permut(nums, ds, ans, freq);
-                freq[i] = 0;
-                ds.pop_back();    
-            }
+        for(int i = idx; i < nums.size(); i++){
+            swap(nums[idx], nums[i]);
+            recurPermut(idx+1, nums, ans);
+            swap(nums[i], nums[idx]);
         }
     }
 
     vector<vector<int>> permute(vector<int>& nums) {
         vector<vector<int>> ans;
-        vector<int> ds;
-        vector<int> freq(nums.size(), 0);
-        permut(nums, ds, ans, freq);
+        recurPermut(0, nums, ans);
         return ans;
     }
 };
