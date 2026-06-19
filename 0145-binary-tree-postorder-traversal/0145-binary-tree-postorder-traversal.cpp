262665073741cf97bcb613e317235bbce167fbee
KESHAV MUNDEL <keshavmundel076@gmail.com>
2026-06-19 22:17:20 +0530

    Time: 0 ms (100%), Space: 10.9 MB (72.85%) - LeetHub

--- 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.cpp
+++ 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.cpp
@@ -1,39 +1,32 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
 public:
     vector<int> postorderTraversal(TreeNode* root) {
         vector<int> post;
-        if(root == NULL) return post;
+        stack<TreeNode*> st;
+        TreeNode* curr = root;
 
-        stack<TreeNode*> st1;
-        stack<TreeNode*> st2;
-        st1.push(root);
-
-        while(!st1.empty()){
-            root = st1.top();
-            st1.pop();
-            st2.push(root);
-            
-            if(root -> left != NULL){
-                st1.push(root -> left);
+        while (curr != NULL || !st.empty()) {
+            if (curr != NULL) {
+                st.push(curr);
+                curr = curr->left;
             }
-            if(root -> right != NULL){
-                st1.push(root -> right);
+            else {
+                TreeNode* temp = st.top()->right;
+                if (temp == NULL) {
+                    temp = st.top();
+                    st.pop();
+                    post.push_back(temp->val);
+                    while (!st.empty() &&
+                           temp == st.top()->right) {
+                        temp = st.top();
+                        st.pop();
+                        post.push_back(temp->val);
+                    }
+                }
+                else {
+                    curr = temp;
+                }
             }
-        }
-        while(!st2.empty()){
-            post.push_back(st2.top()->val);
-            st2.pop();
         }
         return post;
     }
