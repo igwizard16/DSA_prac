ba255ff0dfcdf3f5f1f7b33d7ea259855745d835
KESHAV MUNDEL <keshavmundel076@gmail.com>
2026-07-20 00:21:58 +0530

    Time: 0 ms (100%), Space: 9.2 MB (84.28%) - LeetHub

---
+++ 0316-remove-duplicate-letters/0316-remove-duplicate-letters.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    string removeDuplicateLetters(string s) {
+        int n = s.length();
+
+        string res;
+        vector<bool> vis(26, false);
+        vector<int> l_idx(26);
+
+        for(int i = 0; i < n; i++){
+            char ch = s[i];
+            l_idx[ch - 'a'] = i;
+        }
+
+        for(int i = 0; i < n; i++){
+            char ch = s[i];
+            int idx = ch - 'a';
+
+            if(vis[idx] == true) continue;
+            while(res.length() > 0 && res.back() > ch && l_idx[res.back() - 'a'] > i){
+                vis[res.back() - 'a'] = false;
+                res.pop_back();
+            }
+            res.push_back(ch);
+            vis[idx] = true;
+        }
+        return res;
+    }
+};
