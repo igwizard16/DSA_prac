de901c1cd430d072bc6f066cd8f216084431120e
KESHAV MUNDEL <keshavmundel076@gmail.com>
2026-07-29 03:42:39 +0530

    Time: 3 ms (98.83%), Space: 128.1 MB (97.44%) - LeetHub

--- 2385-amount-of-time-for-binary-tree-to-be-infected/2385-amount-of-time-for-binary-tree-to-be-infected.cpp
+++ 2385-amount-of-time-for-binary-tree-to-be-infected/2385-amount-of-time-for-binary-tree-to-be-infected.cpp
@@ -11,79 +11,39 @@
  */
 class Solution {
 public:
-    void mark_parent(TreeNode* root, unordered_map<TreeNode*, TreeNode*>& parent_track){
-        queue<TreeNode*> q;
-        q.push(root);
+    int ans = 0;
 
-        while(!q.empty()){
-            TreeNode* curr = q.front(); q.pop();
-            if(curr -> left){
-                q.push(curr -> left);
-                parent_track[curr -> left] = curr;
-            }
-            if(curr -> right){
-                q.push(curr -> right);
-                parent_track[curr -> right] = curr;
-            }
+    int dfs(TreeNode* root, int start) {
+        if (root == nullptr)
+            return 0;
+
+        int left = dfs(root->left, start);
+        int right = dfs(root->right, start);
+
+        // Found the start node
+        if (root->val == start) {
+            ans = max(left, right);
+            return -1;
         }
-    } 
+
+        // Start not present in either subtree
+        if (left >= 0 && right >= 0) {
+            return max(left, right) + 1;
+        }
+
+        // Start is in left subtree
+        if (left < 0) {
+            ans = max(ans, -left + right);
+            return left - 1;
+        }
+
+        // Start is in right subtree
+        ans = max(ans, -right + left);
+        return right - 1;
+    }
 
     int amountOfTime(TreeNode* root, int start) {
-        if(root == NULL) return 0;
-        unordered_map<TreeNode*, TreeNode*> parent_track;
-        mark_parent(root, parent_track);
-        
-        //finding the start and converting into node
-        TreeNode* start_node = NULL;
-        queue<TreeNode*> q;
-        q.push(root);
-        
-        while(!q.empty()){
-            TreeNode* curr = q.front(); q.pop();
-            if(curr -> val == start){
-                start_node = curr;
-                break;
-            }
-            if(curr -> left){
-                q.push(curr -> left);
-            }
-            if(curr -> right){
-                q.push(curr -> right);
-            }
-        }
-
-        unordered_map<TreeNode*, bool> visited;
-        queue<TreeNode* > bfs;
-
-        bfs.push(start_node);
-        visited[start_node] = true; 
-        int minutes = 0;
-
-        while(!bfs.empty()){
-            int size = bfs.size();
-            bool infected = false;
-
-            for(int i = 0; i < size; i++){
-                TreeNode* curr = bfs.front(); bfs.pop();
-                if(curr -> left && !visited[curr -> left]){
-                    bfs.push(curr -> left);
-                    visited[curr -> left] = true;
-                    infected = true;
-                }
-                if(curr -> right && !visited[curr -> right]){
-                    bfs.push(curr -> right);
-                    visited[curr -> right] = true;
-                    infected = true;
-                }
-                if(parent_track.count(curr) && !visited[parent_track[curr]]){
-                    bfs.push(parent_track[curr]);
-                    visited[parent_track[curr]] = true;
-                    infected = true;
-                }
-            }
-            if(infected) minutes++;
-        }
-
-        return minutes;
+        dfs(root, start);
+        return ans;
     }
 };
