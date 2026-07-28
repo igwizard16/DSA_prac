2a67b6585e3a1884c68c70275e755e8792c7b22f
KESHAV MUNDEL <keshavmundel076@gmail.com>
2026-07-29 01:43:39 +0530

    Time: 19 ms (91.11%), Space: 54.2 MB (91.76%) - LeetHub

--- 3517-smallest-palindromic-rearrangement-i/3517-smallest-palindromic-rearrangement-i.cpp
+++ 3517-smallest-palindromic-rearrangement-i/3517-smallest-palindromic-rearrangement-i.cpp
@@ -2,13 +2,23 @@ class Solution {
 public:
     string smallestPalindrome(string s) {
         int n = s.size();
+        int freq[26] = {0};
+        
+        //store freq of each char from half of the string
+        for(int i = 0; i < n/2; i++){
+            freq[s[i] - 'a']++;
+        }
 
-        int mid = n/2;
-        //sorting the mirror image 
-        sort(s.begin(), s.begin() + mid);
+        int left = 0;
+        int right = n - 1;
 
-        for(int i = 0; i < mid; i++){
-            s[n - i - 1] = s[i];
+        for(int i = 0; i < 26; i++){
+            while(freq[i] > 0){
+                char c = i + 'a';
+                s[left++] = c;
+                s[right--] = c;
+                freq[i]--;
+            }
         }
         return s;
     }
