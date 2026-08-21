839ed2d4c10dc428e012636a9afd919617db99b0
KESHAV MUNDEL <keshavmundel076@gmail.com>
2026-08-22 01:26:59 +0530

    Time: 0 ms (100%), Space: 10.5 MB (66.89%) - LeetHub

--- 2073-time-needed-to-buy-tickets/2073-time-needed-to-buy-tickets.cpp
+++ 2073-time-needed-to-buy-tickets/2073-time-needed-to-buy-tickets.cpp
@@ -1,22 +1,15 @@
 class Solution {
 public:
     int timeRequiredToBuy(vector<int>& tickets, int k) {
-        int n = tickets.size();
-        queue<pair<int, int>> q;
-
-        for(int i = 0; i < n; i++){
-            q.push({i, tickets[i]});
-        }
-        
         int time = 0;
 
-        while(!q.empty()){
-            auto[idx, ticket] = q.front();
-            q.pop();
-            ticket--;
-            time++;
-            if(ticket == 0 && idx == k) return time;
-            if(ticket > 0) q.push({idx, ticket});
+        for(int i = 0; i < tickets.size(); i++) {
+            if(i <= k) {
+                time += min(tickets[i], tickets[k]);
+            }
+            else {
+                time += min(tickets[i], tickets[k] - 1);
+            }
         }
         return time;
     }
