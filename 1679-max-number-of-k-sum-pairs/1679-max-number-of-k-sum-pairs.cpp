f52a45757c203b6f439c905832c35e543d64baae
KESHAV MUNDEL <keshavmundel076@gmail.com>
2026-08-22 21:31:47 +0530

    Time: 127 ms (11.49%), Space: 76.8 MB (8.33%) - LeetHub

--- 1679-max-number-of-k-sum-pairs/1679-max-number-of-k-sum-pairs.cpp
+++ 1679-max-number-of-k-sum-pairs/1679-max-number-of-k-sum-pairs.cpp
@@ -5,20 +5,14 @@ public:
         unordered_map<int, int> freq;
         int operations = 0;
 
-        for(int i = 0; i < n; i++){
-            freq[nums[i]]++;
-        }
-
         for(int x : nums){
-            if(freq[x] == 0) continue;
-            int pair = k - x;
+            int need = k - x;
 
-            if(freq[pair] == 0) continue;
-            if(x == pair && freq[x] < 2) continue;
-
-            freq[x]--;
-            freq[pair]--;
-            operations++;
+            if(freq[need] > 0){
+                freq[need]--;
+                operations++;
+            }
+            else freq[x]++;
         }
         return operations;
     }
