38a72ea4fe134ad4c1a22551d6f5e364a98619fc
KESHAV MUNDEL <keshavmundel076@gmail.com>
2026-08-24 21:59:44 +0530

    Time: 0 ms (100%), Space: 9.8 MB (79.72%) - LeetHub

--- 1046-last-stone-weight/1046-last-stone-weight.cpp
+++ 1046-last-stone-weight/1046-last-stone-weight.cpp
@@ -1,55 +1,17 @@
 class Solution {
 public:
-    void find_stones(vector<int>& stones, int& maxi, int& sec_largest, int& idx_1, int& idx_2){
-        int n = stones.size();
-        maxi = -1;
-        sec_largest = -1;
-        idx_1 = -1;
-        idx_2 = -1;
-        for(int i = 0; i < n; i++){
-            if(stones[i] >= maxi) {
-                maxi = stones[i];
-                idx_1 = i;
-            }
-        }
-
-        for(int i = 0; i < n; i++){
-            if(i != idx_1 && stones[i] >= sec_largest) {
-                sec_largest = stones[i];
-                idx_2 = i;
-            }
-        }
-    }
-
     int lastStoneWeight(vector<int>& stones) {
-        int maxi, sec_largest;
-        int idx_1, idx_2;
-        int n = stones.size();
 
         while(stones.size() > 1){
-            find_stones(stones, maxi, sec_largest, idx_1, idx_2);
-        
-            if(sec_largest == maxi){
-                if(idx_1 > idx_2){
-                    stones.erase(stones.begin() + idx_1);
-                    stones.erase(stones.begin() + idx_2);
-                }
-                else{
-                    stones.erase(stones.begin() + idx_2);
-                    stones.erase(stones.begin() + idx_1);
-                }
-            }
-            else{
-                int new_weight = maxi - sec_largest;
-                if(idx_1 > idx_2) {
-                    stones.erase(stones.begin() + idx_1);
-                    stones.erase(stones.begin() + idx_2);
-                }
-                else {
-                    stones.erase(stones.begin() + idx_2);
-                    stones.erase(stones.begin() + idx_1);
-                }
-                stones.push_back(new_weight);
+            sort(stones.begin(), stones.end());
+            int y = stones.back();
+            stones.pop_back();
+
+            int x = stones.back();
+            stones.pop_back();
+
+            if(x != y){
+                stones.push_back(y - x);
             }
         }
         if(stones.empty()) return 0;
