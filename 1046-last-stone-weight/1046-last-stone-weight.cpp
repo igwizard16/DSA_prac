a90a02ad7de2037e12d942a19f6b07a9cd017934
KESHAV MUNDEL <keshavmundel076@gmail.com>
2026-08-24 22:08:43 +0530

    Time: 0 ms (100%), Space: 10 MB (51.07%) - LeetHub

--- 1046-last-stone-weight/1046-last-stone-weight.cpp
+++ 1046-last-stone-weight/1046-last-stone-weight.cpp
@@ -1,20 +1,22 @@
 class Solution {
 public:
     int lastStoneWeight(vector<int>& stones) {
+        priority_queue<int> pq;
 
-        while(stones.size() > 1){
-            sort(stones.begin(), stones.end());
-            int y = stones.back();
-            stones.pop_back();
-
-            int x = stones.back();
-            stones.pop_back();
-
-            if(x != y){
-                stones.push_back(y - x);
-            }
+        for(int stone : stones){
+            pq.push(stone);
         }
-        if(stones.empty()) return 0;
-        return stones[0];
+
+        while(pq.size() > 1){
+            int y = pq.top();
+            pq.pop();
+
+            int x = pq.top();
+            pq.pop();
+
+            if(x != y) pq.push(y - x);
+        }
+        if(pq.empty()) return 0;
+        return pq.top();
     }
 };
