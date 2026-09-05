c73684c3e8ce004db5de57480c2d1392145327e6
KESHAV MUNDEL <keshavmundel076@gmail.com>
2026-09-05 11:55:20 +0530

    Time: 19 ms (25.11%), Space: 210.8 MB (56.49%) - LeetHub

---
+++ 3904-smallest-stable-index-ii/3904-smallest-stable-index-ii.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int firstStableIndex(vector<int>& nums, int k) {
+        int n = nums.size();
+        
+        vector<int> maxi(n, -1);
+        vector<int> mini(n, -1);
+
+        //suffix min
+        mini[n - 1] = nums[n - 1];
+        for(int i = n - 2; i >= 0; i--){
+            mini[i] = min(mini[i + 1], nums[i]);
+        }
+
+        //prefix max
+        maxi[0] = nums[0];
+        for(int i = 1; i < n; i++){
+            maxi[i] = max(maxi[i - 1], nums[i]);
+        }
+
+        for(int i = 0; i < n; i++){
+            if(maxi[i] - mini[i] <= k){
+                return i;
+            }
+        }
+        return -1;
+    }
+};
