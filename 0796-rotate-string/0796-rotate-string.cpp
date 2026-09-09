ac9717e58eee0f4b93ec7946e460a62be696cecd
KESHAV MUNDEL <keshavmundel076@gmail.com>
2026-09-09 22:54:13 +0530

    Time: 0 ms (100%), Space: 8.2 MB (23.46%) - LeetHub

--- 0796-rotate-string/0796-rotate-string.cpp
+++ 0796-rotate-string/0796-rotate-string.cpp
@@ -1,12 +1,8 @@
 class Solution {
 public:
     bool rotateString(string s, string goal) {
-        if(s.length() != goal.length()) return false;
-
-        for(int i = 0; i < s.length(); i++){
-            string rotated = s.substr(i) + s.substr(0, i);
-            if(rotated == goal) return true;
-        }
-        return false;
+        if (s.size() != goal.size()) return false;
+        
+        return (s + s).find(goal) != string::npos;
     }
 };
